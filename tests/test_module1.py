1baffade7a368d013db21e216f14c2243202601c
k.sivasankar.eee@gmail.com <chitty>
2024-10-26 15:31:05 +0530

    Add tests directory

---
+++ tests/test_module1.py
@@ -0,0 +1,4 @@
+from my_project import module1
+
+def test_function1():
+    assert module1.function1() is None  # Example assertion
---
+++ tests/test_module2.py
@@ -0,0 +1,4 @@
+from my_project import module2
+
+def test_function1():
+    assert module2.function1() is None  # Example assertion
